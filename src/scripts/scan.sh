036f88ebe0fcb73f3bcc4fa5e28a033f6d54971f
ostorlab <support@ostorlab.co>
2023-07-10 17:32:50 +0100

    Add ostorlab orb source

---
+++ src/scripts/scan.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ostorlab --api-key="${OSTORLAB_API_KEY}" ci-scan run --log-flavor=circleci --title="${SCAN_TITLE}" --scan-profile="${SCAN_PROFILE}" --break-on-risk-rating="${BREAK_ON_RISK_RATING}" --max-wait-minutes="${MAX_WAIT_MINUTES}" "${EXTRA}" "${ASSET_TYPE}" "${TARGET}"
