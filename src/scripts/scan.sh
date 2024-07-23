3cc479f838760ad77df2afc3a41b3df2de4ff50b
benyissa <benyissa@ostorlab.dev>
2024-07-23 16:20:31 +0100

    Make extra arg optional

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ostorlab --api-key="${OSTORLAB_API_KEY}" ci-scan run --log-flavor=circleci --title="${SCAN_TITLE}" --scan-profile="${SCAN_PROFILE}" --break-on-risk-rating="${BREAK_ON_RISK_RATING}" --max-wait-minutes="${MAX_WAIT_MINUTES}" "${EXTRA}" "${ASSET_TYPE}" "${TARGET}"
+ostorlab --api-key="${OSTORLAB_API_KEY}" ci-scan run --log-flavor=circleci --title="${SCAN_TITLE}" --scan-profile="${SCAN_PROFILE}" --break-on-risk-rating="${BREAK_ON_RISK_RATING}" --max-wait-minutes="${MAX_WAIT_MINUTES}" ${EXTRA:+${EXTRA}} "${ASSET_TYPE}" "${TARGET}"
